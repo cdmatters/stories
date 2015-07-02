a87e5856cc51a35ac345aef46509ac2576f0f3ba
Eric Hambro <eric.hambro@gmail.com>
2015-07-02 19:22:15 +0100

    can add messages

--- haplo/views.py
+++ haplo/views.py
@@ -1,15 +1,26 @@
-from flask import render_template, g
+from flask import render_template, g, request, redirect, url_for
 from haplo import app, connect_db
 
 def get_stories_from_id(parent_id):
     db = connect_db()
-    cur = db.cursor()
+    cur = db.cursor() #user stuff to come
     cur.execute('SELECT message, id FROM entries WHERE parent = ? ORDER BY id ASC', (parent_id,) )
     return [ {'id':row[0],'message':row[1]} for row in cur.fetchall() ]
 
 
+
+
 @app.route('/')
 def index():
-    get_stories_from_id(parent_id=0)
-    return render_template('show_story.html')
+    print get_stories_from_id(parent_id=0)
+    return render_template('show_story.html' )
+
+@app.route('/add', methods=['POST'])
+def add_storyline():
+    db = connect_db()
+    cur = db.cursor()
+    cur.execute('INSERT INTO entries (children, message, parent, user_id) VALUES (?,?,?,0)', 
+                (None, request.form['message'], request.form['parent'] ))
+    db.commit()
+    return redirect( url_for('index'))
 
